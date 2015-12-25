26d02d8804aee1812f87b079dad55caf2519a46e
willy <w.yunnal@gmail.com>
2015-12-25 15:34:32 +0700

    complete request

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -1,4 +1,4 @@
-class HomeController < ApplicationController
+class LeadsController < ApplicationController
 
 
 	def create
--- app/models/lead.rb
+++ app/models/lead.rb
@@ -3,26 +3,34 @@ class Lead < ActiveRecord::Base
 
  
 
-  validate :phone_must_be_present 
+    validate :phone_must_be_present , :valid_selected_product
 
-  def phone_must_be_present
-    if not phone_number.present?
-      self.errors.add(:phone_number, "Nomor telpon harus ada")
-      return self 
+    def phone_must_be_present
+        if not phone_number.present?
+          self.errors.add(:phone_number, "Nomor telpon harus ada")
+          return self 
+        end
     end
-  end
       
+    def valid_selected_product
+        if not  ["1" , "2", "3"].include?( selected_product )
+          self.errors.add(:selected_product, "Salah selected product")
+          return self 
+        end
+    end
 
 	def self.create_object( params )
 		new_object  = self.new 
 
 		new_object.name = params[:name]
-    new_object.email = params[:email]
-    new_object.selected_product = params[:selected_product]
+    	new_object.email = params[:email]
+   		 new_object.selected_product = params[:selected_product]
 		new_object.phone_number = params[:phone_number]
 		new_object.age = params[:age]
 		new_object.address = params[:address]
 		new_object.booking_date = params[:booking_date]
+
+		new_object.selected_product = params[:selected_product]
 
 		new_object.save
 
